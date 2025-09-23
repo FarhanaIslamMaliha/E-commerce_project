d607b5eceebb0d33ca497cc5bf3ad65082265c52
Farhana Islam Maliha <Maliha@Rifats-MacBook-Pro.local>
2025-09-22 23:14:13 -0400

    Contact,About,Checkout done

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,6 @@
 import { useState } from 'react'
-
-import viteLogo from '/vite.svg'
 import './App.css'
 import './index.css'
-import Banner from './components/Banner'
-import Flash_sales from './components/Flash_sales'
-import Categories from './components/Categories'
-// import Products from './components/Products'
-import Experience from './components/Experience'
-import Explore_products from './components/Explore_products'
-import Arrival from './components/Arrival'
-import Customer_Service from './components/Customer_Service'
-import Footer from './components/Footer'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from './RootLayout'
 import Home from './Pages/Home'
@@ -20,6 +9,9 @@ import P_Details from './Pages/P_Details'
 import Wishlist from './Pages/Wishlist'
 import Explore_productsP from './Pages/Explore_productsPage'
 import Cartpage from './Pages/Cartpage'
+import CheckPage from './Pages/CheckPage'
+import AboutPage from './Pages/AboutPage'
+import ContactPage from './Pages/ContactPage';
 
 
 
@@ -37,8 +29,11 @@ const router = createBrowserRouter([
       { path: "Wishlist", Component: Wishlist},
       { path: "P_Details/:id", Component: P_Details},
       { path: "Explore_products", Component: Explore_productsP},
-      {path: "Cart", Component: Cartpage}
-      
+      { path: "Cart", Component: Cartpage},
+      { path:"checkout", Component:CheckPage},
+      { path:"about", Component:AboutPage}, 
+      { path:"Contact", Component: ContactPage}
+       
         ],
       },
       
---
+++ vite-project/src/Pages/CheckPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CheckOut from '../components/CheckOut'
+
+const CheckPage = () => {
+   return (
+    <>
+    <div>
+        <CheckOut/>
+    </div>
+    </>
+
+  )
+}
+
+export default CheckPage
